b2b3e92ab2ff218bfef5e914e386aa452e6f5dda
maxdavid <me@maxdavid.com>
2019-08-21 14:45:17 -0700

    Correct knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,7 @@
-const productionDbConnection = process.env.DATABASE_URL || localPg;
-
-module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: { filename: dbPath },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations'
-    },
-    seeds: { directory: './database/seeds' },
-    // by default SQLite will not enforce foreign keys
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); // enforce FK
-      }
-    }
-  },
-  testing: {
-    client: 'pg',
-    connection: localPgTest,
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: productionDbConnection, // could be an object or a string
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  }
-};
 // Update with your config settings.
+const productionDbConnection = process.env.DATABASE_URL || {
+  filename: './data/projects.db3'
+};
 
 module.exports = {
   development: {
@@ -76,7 +40,7 @@ module.exports = {
   },
   testing: {
     client: 'pg',
-    connection: { database: 'testing_postgres' },
+    connection: { filename: './data/testing_projects.db3' },
     migrations: {
       directory: './database/migrations'
     }
