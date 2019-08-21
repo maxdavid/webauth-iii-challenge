0bcc169578d0cdebd0f980a96ff72fab07858ff0
maxdavid <me@maxdavid.com>
2019-08-21 14:50:13 -0700

    Fix dbconfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-console.log(process.env.DB_ENV);
-module.exports = knex(config[process.env.DB_ENV]);
+const DB_ENV = process.env.DB_ENV || 'development';
+module.exports = knex(config[DB_ENV]);
